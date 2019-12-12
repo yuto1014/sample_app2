6dadc1c2392f2f227e9b577ecff05a0a2a2592f1
Takagi Yuto <oct.yto.14@gmail.com>
2019-12-12 02:00:30 +0000

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,17 +2,17 @@ class SessionsController < ApplicationController
   def new
   end
   
-  def create
-     user = User.find_by(email: params[:session][:email].downcase)
+   def create
+    user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-       log_in user
-       remember user
-      redirect_to user
+      log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      redirect_back_or user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
-  end
+   end
   
   def destroy
     log_out if logged_in?
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,6 +12,11 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
   
+  # 渡されたユーザーがログイン済みユーザーであればtrueを返す
+  def current_user?(user)
+    user == current_user
+  end
+  
   # 記憶トークンcookieに対応するユーザーを返す
   def current_user
     if (user_id = session[:user_id])
@@ -58,6 +63,17 @@ class SessionsHelperTest < ActionView::TestCase
   def setup
     @user = users(:michael)
     remember(@user)
+  end
+  
+   # 記憶したURL (もしくはデフォルト値) にリダイレクト
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # アクセスしようとしたURLを覚えておく
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 
   test "current_user returns right user when session is nil" do
